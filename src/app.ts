166251950a5e3fe11f89911e341657421c8ba47b
Wexop <mattheurtin@gmail.com>
2024-10-31 14:00:23 +0100

    wip

--- src/app.ts
+++ src/app.ts
@@ -19,12 +19,14 @@ app.get( "/", async ( req, res ) => {
     .innerJoin( "qualite-donnees.trips as t", "t.trip_id", "st.trip_id" )
     .innerJoin( "qualite-donnees.routes as r", "t.route_id", "r.route_id" )
     .where( "parent_station", "!=", "" )
-    .select( ["s.stop_name as name", "s.location_type", "r.route_color as color", "s.wheelchair_boarding", "r.route_text_color", "r.route_type as type", "s.parent_station", "r.route_short_name"] )
+    .select( ["s.stop_name as name", "s.location_type", "r.route_color as color", "s.wheelchair_boarding", "r.route_text_color", "r.route_type as type", "s.parent_station", "r.route_short_name", "r.route_long_name"] )
     .groupBy( "s.stop_id" )
     .groupBy( "r.route_color" )
     .groupBy( "r.route_text_color" )
     .groupBy( "r.route_type" )
     .groupBy( "r.route_short_name" )
+    .groupBy( "r.route_long_name" )
+    .distinct()
     .execute()
 
 // Créer un dictionnaire pour associer chaque parent avec ses enfants
